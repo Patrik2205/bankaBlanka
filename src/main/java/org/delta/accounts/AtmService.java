4133a2e35ebdb3af1149b18d9239d7fbbbaeccca
Patrik Znamenáček <Patrik220@email.cz>
2024-10-15 11:05:13 +0200

    zmeny

--- src/main/java/org/delta/accounts/AtmService.java
+++ src/main/java/org/delta/accounts/AtmService.java
@@ -2,4 +2,19 @@ package org.delta.accounts;
 
 public class AtmService {
 
+    public void withdrawMoney(BankAccount bankAccount, double amount) throws NoMoneyOnAccountException {
+        if(bankAccount.getBalance() < amount){
+            throw new NoMoneyOnAccountException("No money");
+        }
+
+        System.out.println("Balance before: " + bankAccount.getBalance());
+        bankAccount.setBalance(bankAccount.getBalance()-amount);
+        System.out.println("Balance after: " + bankAccount.getBalance());
+    }
+
+    public void depositMoney(BankAccount bankAccount, double amount){
+        System.out.println("Balance before: " + bankAccount.getBalance());
+        bankAccount.setBalance(bankAccount.getBalance()+amount);
+        System.out.println("Balance after: " + bankAccount.getBalance());
+    }
 }
